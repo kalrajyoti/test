b82f4a51bc955847b15f10966d2e87d1a9a7a864
kalrajyoti <jyoti.kalra3@gmail.com>
2017-01-03 12:58:33 +0530

    Add files via upload

---
+++ Myclass.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student();
+		s.setId(1);
+		s.setName("jyoti");
+		s.setAge(20);
+		System.out.println(s.getName() +  "  is  " +s.getAge() + " year old");
+
+		
+	}
+
+}
